a1e1b867dced2b92e02b1a0c7db61618d0358976
Álison Fernandes <vvolkgang@users.noreply.github.com>
2023-05-20 22:23:02 +0100

    First Blood!

---
+++ DroidApp/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Androidx.Credentials;
+using Java.Lang;
+using Java.Util.Concurrent;
+
+namespace DroidApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            var manager = CredentialManager.Create(Application.Context);
+            var req = new CreatePasswordRequest("username123", "password123");
+            
+            var cancellationSignal = new Android.OS.CancellationSignal();
+            var executor = new Executor();
+            var callback = new CredCallback();
+            manager.CreateCredentialAsync(req, this, cancellationSignal, executor, callback);
+            
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+        
+        class CredCallback : Java.Lang.Object, ICredentialManagerCallback
+        {
+            public void OnError(Object e)
+            {
+                // todo
+            }
+
+            public void OnResult(Object result)
+            {
+                // todo
+            }
+        }
+        class Executor : Java.Lang.Object, IExecutor
+        {
+            public void Execute(IRunnable command)
+            {
+                //todo
+            }
+        }
+    }
+}
